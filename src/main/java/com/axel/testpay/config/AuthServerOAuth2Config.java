0b70f5aedb5ce3e0e0e1d9792101785a584c2450
Axel <true.axel@gmail.com>
2018-09-12 06:42:26 +0300

    second commit

--- src/main/java/com/axel/testpay/config/AuthServerOAuth2Config.java
+++ src/main/java/com/axel/testpay/config/AuthServerOAuth2Config.java
@@ -1,20 +1,36 @@
 package com.axel.testpay.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.approval.UserApprovalHandler;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthServerOAuth2Config extends AuthorizationServerConfigurerAdapter {
 
+    private static String REALM="TESTPAY_OAUTH_REALM";
+
+    @Autowired
+    private TokenStore tokenStore;
+
+    @Autowired
+    private UserApprovalHandler userApprovalHandler;
+
+    @Autowired
+    @Qualifier("authenticationManagerBean")
+    private AuthenticationManager authenticationManager;
+
     @Override
     public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
-        oauthServer
-                .tokenKeyAccess("permitAll()")
-                .checkTokenAccess("isAuthenticated()");
+        oauthServer.realm(REALM+"/client");
     }
 
     @Override
@@ -23,6 +39,13 @@ public class AuthServerOAuth2Config extends AuthorizationServerConfigurerAdapter
                 .withClient("sampleClientId")
                 .authorizedGrantTypes("client-credentials")
                 .scopes("read", "write")
-                .secret("testSecret");
+                .secret("testSecret")
+                .accessTokenValiditySeconds(300);
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenStore(tokenStore).userApprovalHandler(userApprovalHandler)
+                .authenticationManager(authenticationManager);
     }
 }
